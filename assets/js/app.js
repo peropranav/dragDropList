498a087b17ccf63e01392924884aa9a504e19688
Pranav Kapoor <pranavkapoor99@gmail.com>
2019-02-05 12:37:20 +0530

    adding dragging

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 var add_todo_btn = document.getElementById('add-btn');
 var todo_input = document.getElementById('todo-input');
-var list = document.getElementById('list');
+var list = document.getElementById('columns');
 
 add_todo_btn.addEventListener('click',function(){
     var todo = todo_input.value;
@@ -15,16 +15,21 @@ add_todo_btn.addEventListener('click',function(){
     var todo_text_field = document.createElement('span');
     var edit_btn = document.createElement('button');
     var remove_btn = document.createElement('button');
+
+     //for dragging
+     child_div_list.classList.add('column');
+     child_div_list.draggable=true;
+
     child_div_list.classList.add('child_div_list_class');
     div_btn.classList.add('div_item_class');
     div_item.classList.add('div_btn_class');
     edit_btn.classList.add('edit_btn_class');
     remove_btn.classList.add('remove_btn_class');
 
-
+    
     //edit input
 
-    var edit_input = document.createElement('textareas');
+    var edit_input = document.createElement('textarea');
     edit_input.classList.add('edit_input_class');
     edit_input.classList.add('hide');
     edit_input.name='edit-input';
@@ -82,9 +87,58 @@ div_btn.appendChild(remove_btn);
 edit_btn.appendChild(document.createTextNode('EDIT'));
 remove_btn.appendChild(document.createTextNode('REMOVE'));
 todo_text_field.appendChild(document.createTextNode(todo));    
-   
+addEventsDragAndDrop(child_div_list);
 });
 
 
-
-
+function dragStart(e) {
+    this.style.opacity = '0.4';
+    dragSrcEl = this;
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+  };
+  
+  function dragEnter(e) {
+    this.classList.add('over');
+  }
+  
+  function dragLeave(e) {
+    e.stopPropagation();
+    this.classList.remove('over');
+  }
+  
+  function dragOver(e) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+    return false;
+  }
+  
+  function dragDrop(e) {
+    if (dragSrcEl != this) {
+      dragSrcEl.innerHTML = this.innerHTML;
+      this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+    return false;
+  }
+  
+  function dragEnd(e) {
+    var listItens = document.querySelectorAll('.draggable');
+    [].forEach.call(listItens, function(item) {
+      item.classList.remove('over');
+    });
+    this.style.opacity = '1';
+  }
+  
+  function addEventsDragAndDrop(el) {
+    el.addEventListener('dragstart', dragStart, false);
+    el.addEventListener('dragenter', dragEnter, false);
+    el.addEventListener('dragover', dragOver, false);
+    el.addEventListener('dragleave', dragLeave, false);
+    el.addEventListener('drop', dragDrop, false);
+    el.addEventListener('dragend', dragEnd, false);
+  }
+  
+  var listItens = document.querySelectorAll('.draggable');
+  [].forEach.call(listItens, function(item) {
+    addEventsDragAndDrop(item);
+  });
